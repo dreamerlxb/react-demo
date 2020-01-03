9c3a674b819b85d3701aeaa7fa29422d0799a05b
lixuebiao <lixuebiao1115@126.com>
2020-01-03 21:04:05 +0800

    ===ok===2

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -188,6 +188,22 @@ module.exports = {
               },
             ],
           },
+          {
+            test: /\.less$/,
+            use: [
+              'style-loader',
+              "css-loader",
+              "less-loader"
+            ]
+          },
+          {
+            test: /\.(scss|sass)$/,
+            use: [
+              'style-loader',
+              "css-loader",
+              "sass-loader"
+            ]
+          },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
           // In production, they would get copied to the `build` folder.
